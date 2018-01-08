af07f1d4885fd40b1f96fb2058a716e3adc38420
EmadBrown <emadbrown2013@gmail.com>
2018-01-08 21:50:05 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>About</h1>
+@endsection
+
+
+@section('sidebar')
+  <p> This is appended to the sidebar</p>
+@endsection
---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,7 @@
+
+@section('sidebar')
+<div class="sidebar">
+    <h3>sidebar</h3>
+    <p>This is sidebar</p>
+</div>
+@show
